319a0b1f3302f13b470bc2f17f4eb60805eac2e9
Art Cancro <ajc@citadel.org>
2019-08-20 20:12:04 -0400

    oops

--- citadel/utils/setup.c
+++ citadel/utils/setup.c
@@ -959,19 +959,17 @@ void fixnss(void) {
  * We keep them here so we don't lose the translations if we need them later.
  */
 void niu_messages() {
-				important_message(_("Setup finished"),
-						  _("Setup of the Citadel server is complete.\n"
-						    "If you will be using WebCit, please run its\n"
-						    "setup program now; otherwise, run './citadel'\n"
-						    "to log in.\n"));
-			important_message(_("Setup failed"),
-					  _("Setup is finished, but the Citadel server failed to start.\n"
-					    "Go back and check your configuration.\n")
-		important_message(_("Setup finished"),
-				  _("Setup is finished.  You may now start the server."));
+important_message(_("Setup finished"),
+_("Setup of the Citadel server is complete.\n"
+"If you will be using WebCit, please run its\n"
+"setup program now; otherwise, run './citadel'\n"
+"to log in.\n"));
+important_message(_("Setup failed"),
+_("Setup is finished, but the Citadel server failed to start.\n"
+"Go back and check your configuration.\n");
+important_message(_("Setup finished"),
+_("Setup is finished.  You may now start the server."));
 }
-
-
 
 
 int main(int argc, char *argv[])
