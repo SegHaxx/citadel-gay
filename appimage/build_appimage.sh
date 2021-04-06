47fa232ed888bbe5b62ab361b64cc4be78359ab5
Art Cancro <ajc@citadel.org>
2021-04-06 11:20:07 -0400

    Added more exit points to build_appimage.sh so we know where it failed

--- appimage/build_appimage.sh
+++ appimage/build_appimage.sh
@@ -7,28 +7,28 @@ rm -fr $CITADEL_BUILD_DIR $WEBCIT_BUILD_DIR
 # libcitadel has to be built in a "real" library directory
 pushd ../libcitadel || exit 1
 make distclean 2>/dev/null
-./bootstrap || exit 1
-./configure || exit 1
-make || exit 1
-make install || exit 1
+./bootstrap || exit 2
+./configure || exit 3
+make || exit 4
+make install || exit 5
 popd
 
 # Build the Citadel server
-pushd ../citadel || exit 1
+pushd ../citadel || exit 6
 make distclean 2>/dev/null
-./bootstrap || exit 1
-./configure --prefix=$CITADEL_BUILD_DIR || exit 1
-make || exit 1
-make install || exit 1
+./bootstrap || exit 7
+./configure --prefix=$CITADEL_BUILD_DIR || exit 8
+make || exit 9
+make install || exit 10
 popd
 
 # Build WebCit
-pushd ../webcit || exit 1
+pushd ../webcit || exit 11
 make distclean 2>/dev/null
-./bootstrap || exit 1
-./configure --prefix=$WEBCIT_BUILD_DIR || exit 1
-make || exit 1
-make install || exit 1
+./bootstrap || exit 12
+./configure --prefix=$WEBCIT_BUILD_DIR || exit 13
+make || exit 14
+make install || exit 15
 popd
 
 # Clear out any old versions in the AppDir
@@ -49,21 +49,21 @@ ldconfig -v citadel.AppDir/usr/lib
 # Copy over some utilities
 for bin in db_dump db_load db_recover
 do
-	cp `which $bin` citadel.AppDir/usr/bin/
+	cp `which $bin` citadel.AppDir/usr/bin/	|| exit 16
 done
 
 # Install the Citadel Server application tree
-mkdir -p citadel.AppDir/usr/local/citadel
-rsync -va $CITADEL_BUILD_DIR/ ./citadel.AppDir/usr/local/citadel/
+mkdir -p citadel.AppDir/usr/local/citadel || exit 17
+rsync -va $CITADEL_BUILD_DIR/ ./citadel.AppDir/usr/local/citadel/ || exit 18
 
 # Install the WebCit application tree
-mkdir -p citadel.AppDir/usr/local/webcit
-rsync -va $WEBCIT_BUILD_DIR/ ./citadel.AppDir/usr/local/webcit/
+mkdir -p citadel.AppDir/usr/local/webcit || exit 19
+rsync -va $WEBCIT_BUILD_DIR/ ./citadel.AppDir/usr/local/webcit/ || exit 20
 
 # Remove the build directories
 rm -fr $CITADEL_BUILD_DIR $WEBCIT_BUILD_DIR
 
-cc ctdlvisor.c -o citadel.AppDir/usr/bin/ctdlvisor || exit 1
+cc ctdlvisor.c -o citadel.AppDir/usr/bin/ctdlvisor || exit 21
 
 cpu=`uname -p`
 basefilename=citadel-`date +%s`
