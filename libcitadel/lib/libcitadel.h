bffaaea7b264bbad0ff7d114955ab6a8419d3787
Art Cancro <ajc@citadel.org>
2022-06-12 21:28:35 -0400

    Release version 956 generated by do-release.sh

--- libcitadel/lib/libcitadel.h
+++ libcitadel/lib/libcitadel.h
@@ -26,7 +26,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-#define LIBCITADEL_VERSION_NUMBER 955
+#define LIBCITADEL_VERSION_NUMBER 956
 
 /*
  * Here's a bunch of stupid magic to make the MIME parser portable.
