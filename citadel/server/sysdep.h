1d30a78b6c73a650c21c3ed9564158c8db6e1909
Art Cancro <ajc@citadel.org>
2022-06-27 21:52:58 -0400

    Removed more old artifacts from autoconf's reign of terror.

--- citadel/server/sysdep.h
+++ citadel/server/sysdep.h
@@ -1,236 +1,44 @@
-
+// This file contains some definitions left over from autoconf's reign of terror.
+// We will be slowly phasing this out.
 
 #ifndef CTDLDIR
 #error CTDLDIR is not defined , did we not run configure ?
 #endif
 
-/* whether we have NLS support */
-#define ENABLE_NLS /**/
-
-/* whats the matching format string for pid_t? */
+#define ENABLE_NLS
 #define F_PID_T "%d"
-
-/* whats the matching format string for uid_t? */
 #define F_UID_T "%d"
-
-/* whats the matching format string for xpid_t? */
 #define F_XPID_T "%x"
-
-/* Define to 1 if the `getpgrp' function requires zero arguments. */
 #define GETPGRP_VOID 1
-
-/* Define to 1 if you have the <arpa/nameser_compat.h> header file. */
 #define HAVE_ARPA_NAMESER_COMPAT_H 1
-
-/* Define to 1 if you have the <arpa/nameser.h> header file. */
-#define HAVE_ARPA_NAMESER_H 1
-
-/* Define to 1 if you have the `connect' function. */
-#define HAVE_CONNECT 1
-
-/* Define to 1 if you have the <db.h> header file. */
-#define HAVE_DB_H 1
-
-/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
-   */
-#define HAVE_DIRENT_H 1
-
-/* Define to 1 if you have the <fcntl.h> header file. */
-#define HAVE_FCNTL_H 1
-
-/* Define to 1 if you have the `gethostbyname' function. */
-#define HAVE_GETHOSTBYNAME 1
-
-/* Define to 1 if you have the `getloadavg' function. */
-#define HAVE_GETLOADAVG 1
-
-/* Define to 1 if you have the `getpwnam_r' function. */
 #define HAVE_GETPWNAM_R 1
-
-/* Define to 1 if you have the `getpwuid_r' function. */
 #define HAVE_GETPWUID_R 1
-
-/* Define to 1 if you have the `getspnam' function. */
 #define HAVE_GETSPNAM 1
-
-/* Define to 1 if you have the `gettext' function. */
-#define HAVE_GETTEXT 1
-
-/* Define to 1 if you have the `getutxline' function. */
-#define HAVE_GETUTXLINE 1
-
-/* whether we have iconv for charset conversion */
-#define HAVE_ICONV /**/
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
-
-/* Define to 1 if you have the `pthread' library (-lpthread). */
-#define HAVE_LIBPTHREAD 1
-
-/* Define to 1 if you have the `rt' library (-lrt). */
-#define HAVE_LIBRT 1
-
-/* Define to 1 if you have the <limits.h> header file. */
-#define HAVE_LIMITS_H 1
-
-/* Define to 1 if you have the <malloc.h> header file. */
-#define HAVE_MALLOC_H 1
-
-/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_ICONV
 #define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the `mkdir' function. */
-#define HAVE_MKDIR 1
-
-/* Define to 1 if you have the `mkfifo' function. */
-#define HAVE_MKFIFO 1
-
-/* Define to 1 if you have the `mktime' function. */
-#define HAVE_MKTIME 1
-
-/* Define to 1 if you have the <netinet/in.h> header file. */
-#define HAVE_NETINET_IN_H 1
-
-/* Define to 1 if you have the <paths.h> header file. */
-#define HAVE_PATHS_H 1
-
-/* define this if you have the pthread_cancel() function */
-#define HAVE_PTHREAD_CANCEL /**/
-
-/* Define to 1 if you have the <pthread.h> header file. */
 #define HAVE_PTHREAD_H 1
-
-/* define this if you have the resolv.h header file. */
-#define HAVE_RESOLV_H /**/
-
-/* Define to 1 if you have the `rmdir' function. */
-#define HAVE_RMDIR 1
-
-/* Define to 1 if you have the `select' function. */
-#define HAVE_SELECT 1
-
-/* Define to 1 if you have the `socket' function. */
-#define HAVE_SOCKET 1
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the `strcasecmp' function. */
-#define HAVE_STRCASECMP 1
-
-/* Define to 1 if you have the `strerror' function. */
-#define HAVE_STRERROR 1
-
-/* Define to 1 if you have the `strftime_l' function. */
-#define HAVE_STRFTIME_L 1
-
-/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_RESOLV_H
 #define HAVE_STRINGS_H 1
-
-/* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
-
-/* Define to 1 if you have the `strncasecmp' function. */
-#define HAVE_STRNCASECMP 1
-
-/* Define to 1 if `tm_gmtoff' is a member of `struct tm'. */
 #define HAVE_STRUCT_TM_TM_GMTOFF 1
-
-/* Define if struct ucred is present from sys/socket.h */
 #define HAVE_STRUCT_UCRED 1
-
-/* Define to 1 if you have the <syscall.h> header file. */
 #define HAVE_SYSCALL_H 1
-
-/* Define to 1 if you have the <syslog.h> header file. */
-#define HAVE_SYSLOG_H 1
-
-/* Define to 1 if you have the <sys/ioctl.h> header file. */
-#define HAVE_SYS_IOCTL_H 1
-
-/* Define to 1 if you have the <sys/prctl.h> header file. */
 #define HAVE_SYS_PRCTL_H 1
-
-/* Define to 1 if you have the <sys/select.h> header file. */
 #define HAVE_SYS_SELECT_H 1
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
-/* Define to 1 if you have the <sys/syscall.h> header file. */
 #define HAVE_SYS_SYSCALL_H 1
-
-/* Define to 1 if you have the <sys/time.h> header file. */
-#define HAVE_SYS_TIME_H 1
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if you have <sys/wait.h> that is POSIX.1 compatible. */
-#define HAVE_SYS_WAIT_H 1
-
-/* Define to 1 if you have the <termios.h> header file. */
-#define HAVE_TERMIOS_H 1
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-
-/* Define to 1 if you have the `uselocale' function. */
-#define HAVE_USELOCALE 1
-
-/* Define to 1 if you have the <utmpx.h> header file. */
-#define HAVE_UTMPX_H 1
-
-/* Define to 1 if you have the <utmp.h> header file. */
-#define HAVE_UTMP_H 1
-
-/* define this if struct utmp has an ut_host member */
-#define HAVE_UT_HOST /**/
-
-/* define this if struct utmp has an ut_type member */
-#define HAVE_UT_TYPE /**/
-
-/* Define to 1 if you have the `vprintf' function. */
-#define HAVE_VPRINTF 1
-
-/* where to find our pot files */
 #define LOCALEDIR "/root/citadel/citadel"
-
-/* Define as the return type of signal handlers (`int' or `void'). */
 #define RETSIGTYPE void
-
-/* The size of `char', as computed by sizeof. */
 #define SIZEOF_CHAR 1
-
-/* The size of `int', as computed by sizeof. */
 #define SIZEOF_INT 4
-
-/* The size of `loff_t', as computed by sizeof. */
 #define SIZEOF_LOFF_T 8
-
-/* The size of `long', as computed by sizeof. */
 #define SIZEOF_LONG 8
-
-/* The size of `short', as computed by sizeof. */
 #define SIZEOF_SHORT 2
-
-/* The size of `size_t', as computed by sizeof. */
 #define SIZEOF_SIZE_T 8
-
-/* were should we put our keys? */
 #define SSL_DIR "/root/citadel/citadel/keys"
-
-/* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
-
-/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
 #define TIME_WITH_SYS_TIME 1
 
-/* Enable GNU extensions on systems that have them.  */
+// Enable GNU extensions on systems that have them.
 #ifndef _GNU_SOURCE
 # define _GNU_SOURCE 1
 #endif
