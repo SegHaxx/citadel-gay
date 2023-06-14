16f2f2b1eb901a5dcd3bd6cb97242ae04756c149
Art Cancro <ajc@citadel.org>
2023-06-13 19:21:56 -0900

    Release version 979 generated by do-release.sh

--- citadel/server/citadel_defs.h
+++ citadel/server/citadel_defs.h
@@ -21,7 +21,7 @@
 #include "typesize.h"
 #include "ipcdef.h"
 
-#define REV_LEVEL 978		// This version
+#define REV_LEVEL 979		// This version
 #define REV_MIN		591	// Oldest compatible database
 #define EXPORT_REV_MIN	931	// Oldest compatible export files
 #define LIBCITADEL_MIN	951	// Minimum required version of libcitadel
--- libcitadel/lib/libcitadel.h
+++ libcitadel/lib/libcitadel.h
@@ -19,7 +19,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-#define LIBCITADEL_VERSION_NUMBER 978
+#define LIBCITADEL_VERSION_NUMBER 979
 
 /*
  * Here's a bunch of stupid magic to make the MIME parser portable.
